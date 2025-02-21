2320ecff954c93f804562eb31bf293321c586c95
Vincent Kurtz <vjkurtz@gmail.com>
2025-02-21 15:29:16 -0600

    re-organize in terms of functions

--- drake_simulate_mjcf.py
+++ drake_simulate_mjcf.py
@@ -15,90 +15,106 @@ from pydrake.systems.framework import DiagramBuilder
 from pydrake.visualization import AddDefaultVisualization
 import time
 
-# Get system arguments for what we should do
-parser = argparse.ArgumentParser(description="Simulate a drake model.")
-parser.add_argument(
-    "--mjcf",
-    type=str,
-    help="Path to the xml file defining the model to simulate.",
-    required=True,
-)
-parser.add_argument(
-    "--visualize",
-    action="store_true",
-    help="Whether to visualize the simulation (try for real-time if true).",
-    default=False,
-    required=False,
-)
-parser.add_argument(
-    "--sim_time",
-    type=float,
-    help="How long to simulate the model for (in seconds).",
-    default=10.0,
-    required=False,
-)
-parser.add_argument(
-    "--hydroelastic",
-    action="store_true",
-    help="Whether to use hydroelastic contact (default is false).",
-    default=False,
-)
-parser.add_argument(
-    "--time_step",
-    type=float,
-    help="Simulator step size dt (in seconds).",
-    default=0.005,
-    required=False,
-)
-args = parser.parse_args()
 
-# Start meshcat
-meshcat = StartMeshcat()
+def run_simulation(xml_file, visualize, sim_time, hydroelastic, time_step):
+    # Start meshcat
+    meshcat = StartMeshcat()
 
-# Set up the system diagram
-builder = DiagramBuilder()
+    # Set up the system diagram
+    builder = DiagramBuilder()
 
-plant, scene_graph = AddMultibodyPlantSceneGraph(builder, args.time_step)
-parser = Parser(plant, scene_graph)
-parser.AddModels(args.mjcf)
-plant.Finalize()
+    plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step)
+    parser = Parser(plant, scene_graph)
+    parser.AddModels(xml_file)
+    plant.Finalize()
 
-if args.hydroelastic:
-    sg_config = SceneGraphConfig()
-    sg_config.default_proximity_properties.compliance_type = "compliant"
-    sg_config.default_proximity_properties.slab_thickness = 0.1
-    scene_graph.set_config(sg_config)
+    if hydroelastic:
+        sg_config = SceneGraphConfig()
+        sg_config.default_proximity_properties.compliance_type = "compliant"
+        sg_config.default_proximity_properties.slab_thickness = 0.1
+        scene_graph.set_config(sg_config)
 
-if args.visualize:
-    AddDefaultVisualization(builder=builder, meshcat=meshcat)
+    if visualize:
+        AddDefaultVisualization(builder=builder, meshcat=meshcat)
 
-diagram = builder.Build()
+    diagram = builder.Build()
 
-# Initialize the simulator
-simulator = Simulator(diagram)
-if args.visualize:
-    simulator.set_target_realtime_rate(1.0)
-    simulator.set_publish_every_time_step(True)
-simulator.Initialize()
+    # Initialize the simulator
+    simulator = Simulator(diagram)
+    if args.visualize:
+        simulator.set_target_realtime_rate(1.0)
+        simulator.set_publish_every_time_step(True)
+    simulator.Initialize()
 
-# Wait for meshcat to be ready
-print(f"Simulating a {plant.num_positions()} DoF model for {args.sim_time} seconds with dt={args.time_step}...")
-if args.visualize:
-    input("Press [ENTER] to continue...")
+    # Wait for meshcat to be ready
+    print(f"Simulating a {plant.num_positions()} DoF model for {sim_time} seconds with dt={time_step}...")
+    if args.visualize:
+        input("Press [ENTER] to continue...")
 
-# Run the simulation
-meshcat.StartRecording()
-start_time = time.time()
-simulator.AdvanceTo(args.sim_time)
-wall_time = time.time() - start_time
-meshcat.StopRecording()
-meshcat.PublishRecording()
+    # Run the simulation
+    meshcat.StartRecording()
+    start_time = time.time()
+    simulator.AdvanceTo(sim_time)
+    wall_time = time.time() - start_time
+    meshcat.StopRecording()
+    meshcat.PublishRecording()
 
-# Print some statistics
-print(f"Wall time: {wall_time:.4f} seconds")
-print(f"Real-time rate: {args.sim_time/wall_time:.4f}x")
-print(f"FPS: {(args.sim_time/args.time_step)/wall_time:.4f}")
+    # Print some statistics
+    rtr = sim_time / wall_time
+    fps = (sim_time/time_step)/wall_time
+    print(f"Wall time: {wall_time:.4f} seconds")
+    print(f"Real-time rate: {rtr:.4f}x")
+    print(f"FPS: {fps:.4f}")
 
-# Wait for meshcat to publish the recording
-if args.visualize:
-    input("Press [ENTER] to exit...")
+    # Wait for meshcat to publish the recording
+    if args.visualize:
+        input("Press [ENTER] to exit...")
+
+    return wall_time, rtr, fps
+
+if __name__=="__main__":
+    # Get system arguments for what we should do
+    parser = argparse.ArgumentParser(description="Simulate a drake model.")
+    parser.add_argument(
+        "--mjcf",
+        type=str,
+        help="Path to the xml file defining the model to simulate.",
+        required=True,
+    )
+    parser.add_argument(
+        "--visualize",
+        action="store_true",
+        help="Whether to visualize the simulation (try for real-time if true).",
+        default=False,
+        required=False,
+    )
+    parser.add_argument(
+        "--sim_time",
+        type=float,
+        help="How long to simulate the model for (in seconds).",
+        default=10.0,
+        required=False,
+    )
+    parser.add_argument(
+        "--hydroelastic",
+        action="store_true",
+        help="Whether to use hydroelastic contact (default is false).",
+        default=False,
+    )
+    parser.add_argument(
+        "--time_step",
+        type=float,
+        help="Simulator step size dt (in seconds).",
+        default=0.005,
+        required=False,
+    )
+    args = parser.parse_args()
+
+    # Run the simulation
+    run_simulation(
+        args.mjcf,
+        args.visualize,
+        args.sim_time,
+        args.hydroelastic,
+        args.time_step,
+    )
--- mujoco_simulate_mjcf.py
+++ mujoco_simulate_mjcf.py
@@ -11,75 +11,94 @@ import mujoco
 import mujoco.viewer
 import time
 
-# Get system arguments for what we should do
-parser = argparse.ArgumentParser(description='Simulate a mujoco model.')
-parser.add_argument(
-    "--mjcf",
-    type=str,
-    help="Path to the xml file defining the model to simulate.",
-    required=True,
-)
-parser.add_argument(
-    "--visualize",
-    action="store_true",
-    help="Whether to visualize the simulation (try for real-time if true).",
-    default=False,
-    required=False,
-)
-parser.add_argument(
-    "--sim_time",
-    type=float,
-    help="How long to simulate the model for (in seconds).",
-    default=10.0,
-    required=False,
-)
-parser.add_argument(
-    "--time_step",
-    type=float,
-    help="Simulator step size dt (in seconds).",
-    default=0.005,
-    required=False,
-)
-args = parser.parse_args()
 
-# Load the model
-model = mujoco.MjModel.from_xml_path(args.mjcf)
-model.opt.timestep = args.time_step
-model.opt.disableactuator = 1   # disable actuators so the robot falls freely
-data = mujoco.MjData(model)
-    
-# Print some statistics
-print(f"Simulating a {model.nq} DoF model for {args.sim_time} seconds with dt={args.time_step}...")
+def run_experiments(mjcf, visualize, sim_time, time_step):
+    # Load the model
+    model = mujoco.MjModel.from_xml_path(mjcf)
+    model.opt.timestep = time_step
+    model.opt.disableactuator = (
+        1  # disable actuators so the robot falls freely
+    )
+    data = mujoco.MjData(model)
 
-if args.visualize:
-    # Set up real-time-ish sim with the interactive visualizer
-    start_time = time.time()
+    # Print some statistics
+    print(
+        f"Simulating a {model.nq} DoF model for {sim_time} seconds with dt={time_step}..."
+    )
 
-    with mujoco.viewer.launch_passive(model, data) as viewer:
-        while data.time < args.sim_time and viewer.is_running():
-            st = time.time()
+    if visualize:
+        # Set up real-time-ish sim with the interactive visualizer
+        start_time = time.time()
 
-            # Step the simulation
+        with mujoco.viewer.launch_passive(model, data) as viewer:
+            while data.time < sim_time and viewer.is_running():
+                st = time.time()
+
+                # Step the simulation
+                mujoco.mj_step(model, data)
+
+                # Update the viewer
+                viewer.sync()
+
+                # Try to run in roughly real-time
+                step_time = time.time() - st
+                if step_time < model.opt.timestep:
+                    time.sleep(model.opt.timestep - step_time)
+
+        wall_time = time.time() - start_time
+
+    else:
+        # Simulate headless and as fast as possible
+        start_time = time.time()
+        while data.time < sim_time:
             mujoco.mj_step(model, data)
+        wall_time = time.time() - start_time
 
-            # Update the viewer
-            viewer.sync()
+    # Calculate statistics
+    real_time_rate = sim_time / wall_time
+    fps = (sim_time / time_step) / wall_time
 
-            # Try to run in roguhly real-time
-            step_time = time.time() - st
-            if step_time < model.opt.timestep:
-                time.sleep(model.opt.timestep - step_time)
+    # Print some statistics
+    print(f"Wall time: {wall_time:.4f} seconds")
+    print(f"Real-time rate: {real_time_rate:.4f}x")
+    print(f"FPS: {fps:.4f}")
 
-    wall_time = time.time() - start_time
+    return wall_time, real_time_rate, fps
 
-else:
-    # Simulate headless and as fast as possible
-    start_time = time.time()
-    while data.time < args.sim_time:
-        mujoco.mj_step(model, data)
-    wall_time = time.time() - start_time
 
-# Print some statistics
-print(f"Wall time: {wall_time:.4f} seconds")
-print(f"Real-time rate: {args.sim_time/wall_time:.4f}x")
-print(f"FPS: {(args.sim_time/args.time_step)/wall_time:.4f}")
+if __name__ == "__main__":
+    # Get system arguments for what we should do
+    parser = argparse.ArgumentParser(description="Simulate a mujoco model.")
+    parser.add_argument(
+        "--mjcf",
+        type=str,
+        help="Path to the xml file defining the model to simulate.",
+        required=True,
+    )
+    parser.add_argument(
+        "--visualize",
+        action="store_true",
+        help="Whether to visualize the simulation (try for real-time if true).",
+        default=False,
+        required=False,
+    )
+    parser.add_argument(
+        "--sim_time",
+        type=float,
+        help="How long to simulate the model for (in seconds).",
+        default=10.0,
+        required=False,
+    )
+    parser.add_argument(
+        "--time_step",
+        type=float,
+        help="Simulator step size dt (in seconds).",
+        default=0.005,
+        required=False,
+    )
+    args = parser.parse_args()
+
+    # Run the simulation
+    wall_time, real_time_rate, fps = run_experiments(
+        args.mjcf, args.visualize, args.sim_time, args.time_step
+    )
